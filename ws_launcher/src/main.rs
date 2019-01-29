eef926d456d0d5e15354610568cad84926147462
Ch. (Chanhwi Choi) <ccwpc@hanmail.net>
2019-01-29 20:51:17 +0900

    initial commit

---
+++ ws_launcher/src/main.rs
@@ -0,0 +1,11 @@
+extern crate yew;
+use yew::prelude::App;
+
+extern crate ws_launcher;
+use ws_launcher::*;
+
+fn main() {
+//    yew::initialize();
+////    App::<Model>::new();
+//    yew::run_loop();
+}
